652a648bc1d387ccf5da1729e536165b94d9e68e
Corentin Girard <corentingirard.dev@gmail.com>
2018-01-20 00:39:59 +0100

    Added switch between tools

--- randomProjects/groupProject/sketch.js
+++ randomProjects/groupProject/sketch.js
@@ -1,72 +1,83 @@
 class Rectangle {
     update() {
-      if (mouseIsPressed) {
-        temp_canvas.clear();
-        temp_canvas.strokeWeight(thickness);
-        temp_canvas.stroke(r.value(), g.value(), b.value());
-        temp_canvas.rect(mouseDownX, mouseDownY, mouseX - mouseDownX, mouseY - mouseDownY);
-        image(temp_canvas, 0, 0);
-      }
+        if (mouseIsPressed) {
+            temp_canvas.clear();
+            temp_canvas.strokeWeight(thickness);
+            temp_canvas.stroke(r.value(), g.value(), b.value());
+            temp_canvas.rect(mouseDownX, mouseDownY, mouseX - mouseDownX, mouseY - mouseDownY);
+            image(temp_canvas, 0, 0);
+        }
     }
-  }
-  
-  class Line {
-    update() {
-      if (mouseIsPressed) {
-        temp_canvas.clear();
-        temp_canvas.strokeWeight(thickness);
-        temp_canvas.stroke(r.value(), g.value(), b.value());
-        temp_canvas.line(mouseDownX, mouseDownY, mouseX, mouseY);
-        image(temp_canvas, 0, 0);
-      }
-    }
-  }
-  
-  var r;
-  var g;
-  var b;
-  var thickness = 5;
+}
 
-  var tool = new Rectangle(); //the current tool
-  
-  var main_canvas, temp_canvas;
-  var mouseDownX, mouseDownY; //the mouse position when we pressed the mouse
-  
-  function mousePressed() {
+class Line {
+    update() {
+        if (mouseIsPressed) {
+            temp_canvas.clear();
+            temp_canvas.strokeWeight(thickness);
+            temp_canvas.stroke(r.value(), g.value(), b.value());
+            temp_canvas.line(mouseDownX, mouseDownY, mouseX, mouseY);
+            image(temp_canvas, 0, 0);
+        }
+    }
+}
+
+var r;
+var g;
+var b;
+var thickness = 5;
+
+var currentTool = 0;
+
+var tools = [
+    new Rectangle(),
+    new Line()
+];
+
+var main_canvas, temp_canvas;
+var mouseDownX, mouseDownY; //the mouse position when we pressed the mouse
+
+function keyPressed() {
+    if (keyCode == TAB) {
+        currentTool = (currentTool + 1) % tools.length;
+    }
+}
+
+function mousePressed() {
     mouseDownX = mouseX;
     mouseDownY = mouseY;
-  }
-  
-  function mouseReleased() {
+}
+
+function mouseReleased() {
     main_canvas.image(temp_canvas, 0, 0); //we apply the object which has been drawn with the current tool to the actual drawing
-  }
-  
-  function setupSliders() {
+}
+
+function setupSliders() {
     r = createSlider(0, 255, 0, 1);
     r.position(10, 10);
-  
+
     g = createSlider(0, 255, 0, 1);
     g.position(10, 50);
-  
+
     b = createSlider(0, 255, 0, 1);
     b.position(10, 90);
 
     //thickness = createSlider(1, 20, 1, 0);
-  }
-  
-  function setup() {
+}
+
+function setup() {
     setupSliders();
-  
+
     createCanvas(window.innerWidth, window.innerHeight);
 
     main_canvas = createGraphics(width, height); //contains the actual drawing
     temp_canvas = createGraphics(width, height); //contains the temporary object being drawn by the current tool
-  
+
     background(255);
-  }
-  
-  function draw() {
+}
+
+function draw() {
     clear();
     image(main_canvas, 0, 0);
-    tool.update();
-  }
+    tools[currentTool].update();
+}
